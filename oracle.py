34a51a29a493a496f414786c09244cd30498df68
camille-004 <dunningcamille@gmail.com>
2021-03-19 03:16:12 -0700

    Implemented Walsh function

--- oracle.py
+++ oracle.py
@@ -1,46 +1,93 @@
 import numpy as np
-from scipy.linalg import hadamard
 
 
-def gen_walsh_hadamard(order):
+def to_binary(n, N):
     """
-    Generate a Walsh-Hadamard matrix of given order
+    Get binary representation of input n
 
-    :param order: order of W-H matrix
-    :return: numpy.ndarray
+    :param n: Integer of which to get binary representation
+    :param N: Length of bitstring
+    :return: str
     """
-    mat = hadamard(order)
-    mat[mat < 0] = 0
-    return mat
+    return bin(n).replace('0b', '').zfill(N)
+
+
+def get_w_hat_t_helper(row_idx, col_idx, N):
+    """
+    Helper function to get entry (t, col_idx) of W-H matrix
+
+    :param row_idx: Row index of W-H matrix to find
+    :param col_idx: Column index of W-H matrix to find
+    :param N: Order of W-H matrix
+    :return: int
+    """
+    row_bin = to_binary(row_idx, N)
+    col_bin = to_binary(col_idx, N)
+
+    row_list = [int(row_bit) for row_bit in list(row_bin[::-1])]
+    col_list = [int(col_bit) for col_bit in list(col_bin[::-1])]
+
+    result = 0
+    for row_bit, col_bit in zip(row_list, col_list):
+        result += row_bit * col_bit
+
+    return int((1 + ((-1) ** result)) / 2)
 
 
 class Oracle:
-    """
-    Oracle who knows x, a binary vector of length N = 2^n and has K entries of
-    ones, and receives input w_hat_t in [0, 1]^N from the rows of W_hat_N,
-    matrix to be designed. Outputs y, where y in {0, 1, ..., K}^m, representing
-    m observations
-    """
+    """Class to generate a Walsh-Hadamard matrix based on Walsh function
+    computation"""
 
-    def __init__(self, x):
-        assert np.log2(len(x)).is_integer()
-        self.x_ = x
+    def __init__(self, x, N):
+        self._x = x
+        self._N = N
+        self.W = self.gen_walsh_hadamard()
 
-    def get_y_t(self, w_hat_t):
+    def get_w_hat_t(self, t_idx: int) -> list:
         """
-        :param w_hat_t: t-th row (in [0, 1]^N) of W_hat_N
-        :return: y_t, the observation vector
-        """
-        return np.multiply(w_hat_t, self.x_)
+        Get t-th row of W-H matrix of order _N
 
-# TODO Other Oracles that knows WH matrix and picks index of w_hat_t that is given in above Oracle, another that
-#  determines if solution is unique
-# Oracle that determines i-th row of Walsh-Hadamard matrix <- find out how to do this without the full matrix
+        :param t_idx: Index of row in W-H matrix
+        :return: list
+        """
+        row = []
+        for i in range(self._N):
+            row.append(get_w_hat_t_helper(t_idx, i, self._N))
+
+        return row
+
+    def get_y_t(self, t_idx):
+        """
+        Get observation vector from self._x and self.w_hat_t
+
+        :return: numpy.ndarray
+        """
+        return np.multiply(self.get_w_hat_t(t_idx), self._x)
+
+    def gen_walsh_hadamard(self):
+        """
+        Generate Walsh-Hadamard matrix of order _N
+
+        :return: list
+        """
+        return [self.get_w_hat_t(i) for i in range(self._N)]
 
 
 if __name__ == '__main__':
-    wh = gen_walsh_hadamard(4)
-    w_t = wh[1]
-    print(w_t)
-    orc = Oracle([1, 1, 0, 0])
-    print(orc.get_y_t(1))
+    orc = Oracle([1, 1, 0, 0], 4)
+    t = 3
+
+    w_hat_t = orc.get_w_hat_t(t)  # [1, 0, 0, 1]
+    print(w_hat_t)
+
+    y_hat_t = orc.get_y_t(t)  # [1, 0, 0, 0]
+    print(y_hat_t)
+
+    wh = orc.gen_walsh_hadamard()
+    '''
+    [[1, 1, 1, 1], 
+     [1, 0, 1, 0], 
+     [1, 1, 0, 0], 
+     [1, 0, 0, 1]]
+    '''
+    print(wh)
